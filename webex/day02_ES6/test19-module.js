499a4f5f8211b5d4e841f6003240e332bf6abac1
yunghun97 <yunghun97@naver.com>
2021-11-06 03:01:28 +0900

    1106

---
+++ webex/day02_ES6/test19-module.js
@@ -0,0 +1,11 @@
+const title = "계산기 모듈";
+
+function add(a,b){
+    return a+b;
+}
+
+function sub(a,b){
+    return a-b;
+}
+// 한번에 export 하기
+export {title, add, sub};   
---
+++ webex/day02_ES6/test20-module.js
@@ -0,0 +1,13 @@
+export function mul(a,b){
+    return a * b;
+}
+
+export default{ // 1개만 가능
+    title: '계산기 모듈',
+    add: function(a,b){
+        return a+b;
+    },
+    sub: function(a,b){
+        return a-b;
+    }
+}
